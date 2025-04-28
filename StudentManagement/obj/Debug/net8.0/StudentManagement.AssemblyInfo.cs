ff3a7b27849c4d7b2361ea01fd1a848d0b483dab
Hasan KÖK <archenemy.42@gmail.com>
2025-04-28 21:26:49 +0300

    update

--- StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1240637e9d5db70fb371fd39873e6f394077cc35")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
